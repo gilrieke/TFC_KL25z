bf30a9847bbaaafcfe07941fa0da014bb34029c3
gilrieke <griekejr@gmail.com>
2015-07-30 18:56:45 -0500

    added functions to camera and servomotor.

--- Sources/main.c
+++ Sources/main.c
@@ -37,12 +37,7 @@
 #include "Servo.h"
 #include "PwmLdd1.h"
 #include "WAIT1.h"
-#include "Motor_Left.h"
-#include "PwmLdd2.h"
-#include "TU2.h"
-#include "Motor_Enable.h"
-#include "BitIoLdd1.h"
-#include "Motor_Right.h"
+#include "Motors.h"
 #include "PwmLdd3.h"
 #include "Camera_Analog.h"
 #include "AdcLdd1.h"
@@ -56,15 +51,17 @@
 #include "PE_Const.h"
 #include "IO_Map.h"
 
-#define SERVO_LEFT 62000
-#define SERVO_HALF 61100
-#define SERVO_RIGHT 60200
+#define SERVO_LEFT 17800
+#define SERVO_RIGHT 19400
 
 #define PIX_MIN 0
 #define PIX_MAX 128
 
-uint8_t Camera_Values[128], u8_Centro = 0;
-byte Camera_Threshold;
+uint8_t Camera_Bin[128], u8_Centro = 0;
+uint8_t Camera_Values[128];
+uint8_t Camera_Threshold;
+word servoduty = 0;
+uint8_t u8_PixelCntr = 0;
 
 
 /* User includes (#include below this line is not maintained by Processor Expert) */
@@ -74,48 +71,31 @@ void delay(uint32_t time){
 	}
 }
 
-void Motor_Left(word time){
-	Motor_Left_SetDutyUS(100-time);
-	return;
-}
-
-void Motor_Right(word time){
-	Motor_Right_SetDutyUS(100-time);
-	return;
-}
-
-void Servo_Duty(word duty){
-	
-	return;
-}
-
 void Read_Camera(void){
 	int i;
-	uint16_t u16_MaxValue = 0, u16_threshold = 0, u16_suma = 0;
-	uint8_t u8_PixelCntr = 0;
+	uint8_t u16_MaxValue = 0, u16_threshold = 0;
+	uint16_t u16_suma = 0;
 	
+	u8_PixelCntr = 0;
 	Camera_SI_SetVal();
-	delay(20);
 	Camera_Clock_SetVal();
-	delay(20);
+	Camera_Analog_Measure(TRUE);	
 	Camera_SI_ClrVal();
-	delay(20);
-	Camera_Clock_ClrVal();
 	for(i=0;i<128;i++){
-		delay(20);
-		Camera_Clock_SetVal();
-		Camera_Analog_Measure(TRUE);	
-		Camera_Analog_GetValue8(&Camera_Values[i]);
-		delay(20);
 		Camera_Clock_ClrVal();
+		Camera_Analog_GetValue8(&Camera_Values[i]);
+		delay(60);
+		Camera_Clock_SetVal();
+		Camera_Analog_Measure(TRUE);
 		
 		if(Camera_Values[i] > u16_MaxValue) {
-			u16_MaxValue  = Camera_Values[i];
+			u16_MaxValue  = (uint16_t)Camera_Values[i];
 		}
-		
 	}
-	
+	Camera_Clock_ClrVal();
+
 	u16_threshold = (u16_MaxValue/20) * 17;
+	
 	for(i=0;i<128;i++){
 		if (Camera_Values[i] > u16_threshold){
 			u16_suma += i;
@@ -127,28 +107,39 @@ void Read_Camera(void){
 		}
 	}
 	
-	u8_Centro = (uint8_t)(u16_suma / u8_PixelCntr);
-	
+	if(u8_PixelCntr < 15)
+		u8_Centro = (uint8_t)(u16_suma / u8_PixelCntr);
+
+	return;
+}
+
+
+void Servo_Line (uint8_t centro){
+	servoduty = (word)(SERVO_LEFT + (((SERVO_RIGHT-SERVO_LEFT)/128)*centro));
+	Servo_SetDutyUS(servoduty);
 	return;
 }
 
 /*lint -save  -e970 Disable MISRA rule (6.3) checking. */
 int main(void)
 /*lint -restore Enable MISRA rule (6.3) checking. */
-{
+ {
   /* Write your local variable definition here */
-	
+	int i;
   /*** Processor Expert internal initialization. DON'T REMOVE THIS CODE!!! ***/
   PE_low_level_init();
   /*** End of Processor Expert internal initialization.                    ***/
   /* Write your code here */
   
   Camera_Analog_Calibrate(TRUE);
-  Motor_Enable_SetVal();
   for(;;) {
 	  	  	  
 	  Read_Camera();
+	  Servo_Line(u8_Centro);
+	  //Servo_SetDutyUS(19500);
 	  WAIT1_Waitms(5);
+	  
+
   }
 
   /*** Don't write any code pass this line, or it will be deleted during code generation. ***/
